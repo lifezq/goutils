94c667c2056b5fdbd58aeba06ac2cee663d8bec3
Ryan <lifezqy@126.com>
2016-08-29 09:49:07 +0800

    disk uuid

---
+++ disk.go
@@ -0,0 +1,35 @@
+package goutils
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func GetDiskUUIDNumber(name string) string {
+
+	n := fmt.Sprintf("--name=%s", name)
+	udevadm, err := exec.LookPath("/sbin/udevadm")
+	if err != nil {
+		return ""
+	}
+
+	out, err := exec.Command(udevadm, "info", "--query=property", n).Output()
+	if err != nil {
+		return ""
+	}
+
+	lines := strings.Split(string(out), "\n")
+	for _, line := range lines {
+
+		values := strings.Split(line, "=")
+		if len(values) < 2 || values[0] != "ID_FS_UUID" {
+			// only get ID_FS_UUID
+			continue
+		}
+
+		return values[1]
+	}
+
+	return ""
+}
