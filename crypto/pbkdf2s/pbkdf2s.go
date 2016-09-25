dfb560c05d7f2384feba8bbc18041ae9e9f027f3
Ryan <lifezqy@126.com>
2016-09-26 00:25:32 +0800

    scrypt hash password

--- crypto/pbkdf2s/pbkdf2s.go
+++ crypto/pbkdf2s/pbkdf2s.go
@@ -2,7 +2,7 @@
 //
 // -------------------------------------------------------------------
 
-package pbkdf2
+package pbkdf2s
 
 import (
 	"bytes"
@@ -12,6 +12,7 @@ import (
 	"io"
 
 	"golang.org/x/crypto/pbkdf2"
+	"golang.org/x/crypto/scrypt"
 )
 
 const (
@@ -22,22 +23,15 @@ const (
 func GenerateFromPassword(password []byte, klen int) ([]byte, error) {
 
 	if len(password) < block_size {
-		return []byte{}, fmt.Errorf("Too short")
+		return nil, fmt.Errorf("Too short")
 	}
 
-	ilen := block_size + len(password)%block_size
-	salt := make([]byte, ilen)
-
-	n, err := io.ReadFull(rand.Reader, salt)
+	salt, err := generateSaltFromPassword(password)
 	if err != nil {
-		return []byte{}, err
+		return nil, err
 	}
 
-	if n != ilen {
-		return []byte{}, fmt.Errorf("Length Not Match")
-	}
-
-	return append(salt, pbkdf2.Key(password, salt, block_iter, klen, sha256.New)...), nil
+	return append(salt, pbkdf2.Key(password, salt, block_iter, klen-len(salt), sha256.New)...), nil
 }
 
 func CompareHashAndPassword(hashed []byte, password []byte) bool {
@@ -58,4 +52,65 @@ func CompareHashAndPassword(hashed []byte, password []byte) bool {
 	}
 
 	return true
+}
+
+func ScryptGenerateFromPassword(password []byte, klen int) ([]byte, error) {
+
+	if len(password) < block_size {
+		return nil, fmt.Errorf("Too short")
+	}
+
+	salt, err := generateSaltFromPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	dk, err := scrypt.Key(password, salt, block_iter, block_size<<1, block_size>>3, klen-len(salt))
+	if err != nil {
+		return nil, err
+	}
+
+	return append(salt, dk...), nil
+}
+
+func ScryptCompareHashAndPassword(hashed []byte, password []byte) bool {
+
+	if len(password) < block_size {
+		return false
+	}
+
+	ilen := block_size + len(password)%block_size
+	if len(hashed) <= ilen {
+		return false
+	}
+
+	salt := hashed[:ilen]
+	dk, err := scrypt.Key(password, salt, block_iter, block_size<<1, block_size>>3, len(hashed)-ilen)
+	if err != nil {
+		return false
+	}
+
+	phash := append(salt, dk...)
+	if !bytes.Equal(hashed, phash) {
+		return false
+	}
+
+	return true
+}
+
+func generateSaltFromPassword(password []byte) ([]byte, error) {
+
+	ilen := block_size + len(password)%block_size
+	salt := make([]byte, ilen)
+
+	n, err := io.ReadFull(rand.Reader, salt)
+	if err != nil {
+		return nil, err
+	}
+
+	if n != ilen {
+		return nil, fmt.Errorf("Length Not Match")
+	}
+
+	return salt, nil
 }
--- crypto/pbkdf2s/pbkdf2s_test.go
+++ crypto/pbkdf2s/pbkdf2s_test.go
@@ -2,26 +2,51 @@
 //
 // -------------------------------------------------------------------
 
-package pbkdf2
+package pbkdf2s
 
 import (
 	"fmt"
 	"testing"
+
+	"github.com/lifezq/goutils/encode"
 )
 
-func TestPbkdf2Hash(t *testing.T) {
+func TestPbkdf2GeneratePassword(t *testing.T) {
 
 	for i := 0; i < 10; i++ {
 
 		password := []byte(fmt.Sprintf("%d.%s", i, "abcdef@123"))
 
-		h, err := GenerateFromPassword(password, 64)
+		h, err := GenerateFromPassword(password, 32)
 		if err != nil {
-			t.Errorf("Hash Error : %s \n", err.Error())
+			t.Errorf("GenerateFromPassword Error : %s \n", err.Error())
 		}
 
 		if !CompareHashAndPassword(h, password) {
 			t.Errorf("CompareHashAndPassword Not Match. hashed:%v password:%v\n", h, password)
 		}
+
+		t.Logf("Pbkdf2 hashed len:%d hash:%v hex:%s\n", len(h), h, encode.HexEncodeToString(h))
 	}
+
+}
+
+func TestScryptGeneratePassword(t *testing.T) {
+
+	for i := 0; i < 10; i++ {
+
+		password := []byte(fmt.Sprintf("%d.%s", i, "abcdef@123"))
+
+		h, err := ScryptGenerateFromPassword(password, 32)
+		if err != nil {
+			t.Errorf("ScryptGenerateFromPassword Error : %s \n", err.Error())
+		}
+
+		if !ScryptCompareHashAndPassword(h, password) {
+			t.Errorf("ScryptCompareHashAndPassword Not Match. hashed:%v password:%v\n", h, password)
+		}
+
+		t.Logf("Scrypt hashed len:%d hash:%v hex:%s\n", len(h), h, encode.HexEncodeToString(h))
+	}
+
 }
