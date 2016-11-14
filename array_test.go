86f6f608b72ffdbb932bcb836ffd49ca8aab0468
Ryan <lifezqy@126.com>
2016-11-14 19:50:41 +0800

    u

--- array_test.go
+++ array_test.go
@@ -1,9 +1,22 @@
+// Copyright 2016 The Goutils Author. All Rights Reserved.
+//
+// -------------------------------------------------------------------
+
 package goutils
 
 import "testing"
 
 func TestArrayRemoveDup(t *testing.T) {
 
-	arr := []interface{}{1, 2, 5, 6, 11, 25, 33, 66, 15, 25, 15, 66}
-	t.Logf("ArrayRemoveDup source:%v triped:%v\n", arr, ArrayRemoveDup(arr))
+	for i := 0; i < 100; i++ {
+
+		arr := []interface{}{1, 2, 5, 6, 11, 25, 33, 66, 15, 25, 15, 66}
+
+		striped := ArrayRemoveDup(arr)
+		if len(striped) != 9 {
+			t.Errorf("ArrayRemoveDup error\n")
+		}
+
+		//t.Logf("ArrayRemoveDup source:%v striped:%v\n", arr, len(striped))
+	}
 }
