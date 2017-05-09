eb315a481d4061d6e13af98e3ae2c09398a5d9ee
Ryan <lifezqy@126.com>
2017-05-09 11:53:44 +0800

    limits

---
+++ math/limits.go
@@ -0,0 +1,15 @@
+package math
+
+const (
+	INT8_MAX  = int8(1<<7 - 1)
+	UINT8_MAX = uint8(1<<8 - 1)
+
+	INT16_MAX  = int16(1<<15 - 1)
+	UINT16_MAX = uint16(1<<16 - 1)
+
+	INT32_MAX  = int32(1<<31 - 1)
+	UINT32_MAX = uint32(1<<32 - 1)
+
+	INT64_MAX  = int64(1<<63 - 1)
+	UINT64_MAX = uint64(1<<64 - 1)
+)
