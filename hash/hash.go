239a0e42722096d1ba6daa138fcc71166a158405
Ryan <lifezqy@126.com>
2017-07-18 12:14:44 +0800

    salt hash

--- hash/hash.go
+++ hash/hash.go
@@ -16,3 +16,18 @@ func StringHash(s string, length uint16) string {
 	md5_val := fmt.Sprintf("%x", md5.Sum([]byte(s)))
 	return md5_val[:length]
 }
+
+func SaltHash(s, salt string, length uint16) string {
+
+	if length < 1 {
+		length = 1
+	} else if length > 32 {
+		length = 32
+	}
+
+	m := md5.New()
+	m.Write([]byte(salt))
+
+	md5_val := fmt.Sprintf("%x", m.Sum([]byte(s)))
+	return md5_val[:length]
+}
