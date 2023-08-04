8f3a23efd0ccfa0b63dadb358b3796573d9ff909
lifezq <lifezqy@126.com>
2023-08-04 16:21:26 +0800

    update

--- regexps/regexps.go
+++ regexps/regexps.go
@@ -9,7 +9,7 @@ import "regexp"
 var (
 	DomainPreg   = regexp.MustCompile(`^[0-9a-zA-Z]+[0-9a-zA-Z.-]*\.[a-zA-Z]{2,4}$`)
 	EmailPreg    = regexp.MustCompile(`^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$`)
-	PhonePreg    = regexp.MustCompile(`^1[358]\d{9}$`)
+	PhonePreg    = regexp.MustCompile(`^1[345789]\d{9}$`)
 	UsernamePreg = regexp.MustCompile(`^[a-zA-Z][0-9a-zA-Z_]{5,29}$`)
 )
 
