766a00a5bfa489ca90e7aff8119430a55f0b97a6
lifezq <>
2026-02-05 21:12:00 +0800

    update

--- bookmark/bookmark_test.go
+++ bookmark/bookmark_test.go
@@ -4,23 +4,17 @@
 
 package bookmark
 
-import (
-	"context"
-	"os"
-	"testing"
-)
-
-func TestBookmarkService_Import(t *testing.T) {
-	file, err := os.Open("path to import bookmark html file")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer file.Close()
-
-	s := NewBookmarkService()
-	folders, err := s.Import(context.Background(), file)
-	if err != nil {
-		t.Fatal(err)
-	}
-	t.Log(len(folders))
-}
+//func TestBookmarkService_Import(t *testing.T) {
+//	file, err := os.Open("path to import bookmark html file")
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	defer file.Close()
+//
+//	s := NewBookmarkService()
+//	folders, err := s.Import(context.Background(), file)
+//	if err != nil {
+//		t.Fatal(err)
+//	}
+//	t.Log(len(folders))
+//}
--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module github.com/lifezq/goutils
 
-go 1.20
+go 1.24.0
+
+toolchain go1.24.12
 
 require (
 	github.com/panjf2000/ants/v2 v2.11.4
-	golang.org/x/crypto v0.9.0
-	golang.org/x/net v0.10.0
+	golang.org/x/crypto v0.47.0
+	golang.org/x/net v0.48.0
 )
 
 require golang.org/x/sync v0.11.0 // indirect
